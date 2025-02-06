80189e075c2f5be9a99a710ab7c92aed6353c3cc
gauravpba <36869757+gauravpba@users.noreply.github.com>
2025-02-05 18:45:15 -0600

    Character Blueprint setup
    Created our Aura character and a goblin character BP

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -5,7 +5,11 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 
